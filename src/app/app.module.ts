51a1e8d46da5c424df643a77f9cdaede256b5127
Hamza Zaidi <hzaidi@frontlinetechnologies.com>
2020-03-30 20:46:37 -0400

    add empty nav bar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import * as fromComponents from './components';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    fromComponents.components
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/index.ts
@@ -0,0 +1,5 @@
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+
+export const components: any[] = [
+    NavBarComponent
+]

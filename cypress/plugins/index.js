fbc686ef5ed75f30d82a4a037293207cb901ad1b
Hamza Zaidi <hzaidi@frontlinetechnologies.com>
2020-04-10 19:02:39 -0400

    Github actions

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -16,6 +16,8 @@
  * @type {Cypress.PluginConfig}
  */
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+  return Object.assign({}, config, {
+    screenshotsFolder: 'cypress/e2e/screenshots',
+    videosFolder: 'cypress/e2e/videos',
+  })
 }

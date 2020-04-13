5fae019b5b6f052acfea9072fdd3189eee83d41d
Hamza Zaidi <hzaidi@frontlinetechnologies.com>
2020-04-12 20:03:27 -0400

    componentanize the skill

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,9 +1,11 @@
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { TypingTextComponent } from './typing-text/typing-text.component';
 import { SkillsComponent } from './skills/skills.component';
+import { SkillComponent } from './skill/skill.component';
 
 export const components: any[] = [
     NavBarComponent,
     TypingTextComponent,
-    SkillsComponent
+    SkillsComponent,
+    SkillComponent
 ]
---
+++ src/app/components/skill/skill.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Skill } from '../../models/skill';
+
+@Component({
+  selector: 'app-skill',
+  templateUrl: './skill.component.html',
+  styleUrls: ['./skill.component.scss']
+})
+export class SkillComponent implements OnInit {
+  @Input() skill: Skill;
+  constructor() { }
+  ngOnInit(): void {
+  }
+
+}

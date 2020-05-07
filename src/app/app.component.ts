e3067ce99ebc4673694f8c1d1e4c02e21a3de832
Hamza Zaidi <hzaidi@frontlinetechnologies.com>
2020-05-06 22:33:00 -0400

    css tweaks and experience boxes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,6 @@ export class AppComponent {
   title = 'hamzadev';
   navItems$: Observable<NavItem[]> = of([
     { icon: 'icofont-home', title: 'Home', location: '/home' },
-    { icon: 'icofont-business-man', title: 'About', location: '/about' },
     { icon: 'icofont-settings-alt', title: 'Resume', location: '/resume' },
     { icon: 'icofont-eye', title: 'Portfolio', location: '/portfolio' },
     { icon: 'icofont-blogger', title: 'Blog', location: '/blog' },
